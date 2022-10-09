4adc05bdfefc95709f6c807ec4222be9faa3a544
Valerij575 <valerij575@gmail.com>
2022-10-09 15:30:01 +0200

    Add BasicAuthBearerToken

---
+++ BasicAuth/BasicAuthBearerToken/BasicAuthenticationHandler.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.AspNetCore.Authentication;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using System;
+using System.Security.Claims;
+using System.Text;
+using System.Text.Encodings.Web;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace BasicAuthBearerToken
+{
+    public class BasicAuthenticationHandler : AuthenticationHandler<AuthenticationSchemeOptions>
+    {
+        public BasicAuthenticationHandler(IOptionsMonitor<AuthenticationSchemeOptions> options,
+            ILoggerFactory logger, UrlEncoder encoder, ISystemClock clock)
+            : base(options, logger, encoder, clock)
+        {
+
+        }
+
+        protected override Task<AuthenticateResult> HandleAuthenticateAsync()
+        {
+            Response.Headers.Add("WWW-Authenticate", "Basic");
+
+            if (!Request.Headers.ContainsKey("Authorization"))
+            {
+                return Task.FromResult(AuthenticateResult.Fail("Authorization Header Missing Attribute"));
+            }
+
+            var authorizationHeader = Request.Headers["Authorization"].ToString();
+            var authHeaderRegex = new Regex("Basic (.*)");
+            if (!authHeaderRegex.IsMatch(authorizationHeader))
+            {
+                return Task.FromResult(AuthenticateResult.Fail("Authorization cod not formatted propertly"));
+            }
+
+            var authBase64 = Encoding.UTF8.GetString(Convert.FromBase64String(authHeaderRegex.Replace(authorizationHeader, "$1")));
+            var authSplit = authBase64.Split(Convert.ToChar(":"), 2);
+            var authUsername = authSplit[0];
+            var authPassword = authSplit[1];
+
+            if(!authUsername.Equals("admin") || !authPassword.Equals("Pa$$word"))
+            {
+                return Task.FromResult(AuthenticateResult.Fail("The username or password is not correct"));
+            }
+
+            var authenticatedUser = new AuthenticatedUser("BasicAuthentication", true, "admin");
+            var claimsPrincipal = new ClaimsPrincipal(new ClaimsIdentity(authenticatedUser));
+
+            return Task.FromResult(AuthenticateResult.Success(new AuthenticationTicket(claimsPrincipal, Scheme.Name)));
+        }
+    }
+}
